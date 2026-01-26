a83d285edfeefc71dfc05ee8bc059533f5d0a6a0
muchlakshay <justnotlakshay@gmail.com>
2026-01-26 18:44:04 +0530

    add slab allocator for small objects

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "memory_pool_cpu.h"
 
 int main() {
+    try {
+        MemoryPoolCPU memory_pool{};
+        int size {10};
+        for (int i = 0; i < 3; i++) {
+            auto* mem {static_cast<int *>(memory_pool.getMem(3*1024))};
+            memory_pool.returnMem(mem);
+        }
+    }catch(std::exception& e) {std::cout<<e.what();}
+
+
 
     return 0;
 }
