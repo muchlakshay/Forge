d19c4c0c8ec880998f6a40d6c2e89403c4c5c9b6
muchlakshay <justnotlakshay@gmail.com>
2026-01-29 19:28:03 +0530

    restructure core/tensor dir

---
+++ core/tensor/Storage/storage_abstract.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "memory_pool_cpu.h"
+
--- core/tensor/storage_abstract.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by MSI on 23-01-2026.
-//
-
-#ifndef STORAGE_ABSTRACT_H
-#define STORAGE_ABSTRACT_H
-
-#endif //STORAGE_ABSTRACT_H
