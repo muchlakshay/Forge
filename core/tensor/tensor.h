da3502c6e20c81ca380c08b8b23095d659701b28
muchlakshay <reduxfusion@gmail.com>
2026-02-02 18:46:39 +0530

    add initializer_list-based value setters and tensor metadata accessors
    ADDED:-
    
    this commit expands the Tensor interface to support structured value initialization and exposes basic metadata accessors required for upcoming operation support
    
    -type aliases for nested std::initializer_list up to 4D, enabling intuitive tensor value initialization from brace-enclosed lists
    
    -overloaded setValues template member functions for 1D–4D initializer lists. These will later route through the dispatcher to handle dtype-specific storage writes safely and efficiently .
    
    -public constructor for creating tensors with explicit shape, dtype, gradient flag, and device.
    
    const accessors for:
    
    -shape
    -strides
    -dtype
    -device
    -need_grads
    
    PURPOSE:-
    
    these changes lay the groundwork for:-
    
    -uer-friendly tensor creation
    -cntralized dtype-aware data population via the upcoming dispatcher system
    -cleaner separation between tensor metadata and backend-specific storage logic
    
    no kernel or dispatcher logic is implemented yet, this commit focuses purely on interface expansion needed for the next development phase.
    
    Refs #8

--- core/tensor/tensor.h
+++ core/tensor/tensor.h
@@ -7,6 +7,15 @@ namespace Forge {
     enum class Device {CPU, CUDA};
 }
 
+template <typename T>
+using init_list_1D = std::initializer_list<T>;
+template <typename T>
+using init_list_2D = std::initializer_list<init_list_1D<T>>;
+template <typename T>
+using init_list_3D = std::initializer_list<init_list_2D<T>>;
+template <typename T>
+using init_list_4D = std::initializer_list<init_list_3D<T>>;
+
 class Forge::Tensor {
     Device m_device{};
     Dtype m_dtype{};
@@ -14,4 +23,22 @@ class Forge::Tensor {
     std::vector<std::size_t> m_shape{}, m_strides{};
     std::size_t m_size{};
     bool m_need_grads{};
+public:
+    explicit Tensor(const std::vector<std::size_t>& shape, Forge::Dtype dtype = Forge::Dtype::float32, bool need_grads = true,
+        Forge::Device device = Forge::Device::CPU);
+
+    template <typename T>
+    void setValues(init_list_1D<T>);
+    template <typename T>
+    void setValues(init_list_2D<T>);
+    template <typename T>
+    void setValues(init_list_3D<T>);
+    template <typename T>
+    void setValues(init_list_4D<T>);
+
+    const auto& need_grads() const {return m_need_grads;} ;
+    const auto& shape() const {return m_shape;}
+    const auto& strides() const {return m_strides;} ;
+    const auto& dtype() const {return m_dtype;} ;
+    const auto& device() const {return m_device;} ;
 };
