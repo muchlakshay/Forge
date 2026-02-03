92518ed2097b1d1c3cfc2f45932adb9c13313708
muchlakshay <reduxfusion@gmail.com>
2026-02-03 20:54:37 +0530

    Correct syntax errors in DISPATCH_ALL_TYPES and introduce to_idx()
    ADDED:-
    
    -to_idx() static member function takes and enum class and converts it into an int that can be used as an index.
    
    CORRECTED:-
    
    -in if(DEVICE==Forge::Device::CUDA) instead of empty block i throw an exception since anything related to CUDA is not implemented yet
    
    Refs #13

--- core/ops/Dispatcher/dispatcher.h
+++ core/ops/Dispatcher/dispatcher.h
@@ -1,14 +1,14 @@
 #pragma once
 #include "kernel_base.h"
 #include "tensor.h"
+#include <Eigen/Dense>
 
 #define DISPATCH_ALL_TYPES(DTYPE, DEVICE, ...)\
-[&] {                                  \
+[&]() {                                  \
 switch (DTYPE) {                       \
-case Forge::DType::float16: {          \
+case Forge::Dtype::float16: {          \
 if(DEVICE==Forge::Device::CUDA){       \
-using scalar_t = __half;               \
-__VA_ARGS__();                         \
+throw std::runtime_error("float16 CUDA not implemented"); \
 }                                      \
 else {                                 \
 using scalar_t = Eigen::half;          \
@@ -16,27 +16,27 @@ using scalar_t = Eigen::half;          \
 }                                      \
 break ;                                \
 }                                      \
-case Forge::DType::float32: {          \
+case Forge::Dtype::float32: {          \
 using scalar_t = float;                \
 __VA_ARGS__();                         \
 break;                                 \
 }                                      \
-case Forge::DType::float64: {          \
+case Forge::Dtype::float64: {          \
 using scalar_t = double;               \
 __VA_ARGS__();                         \
 break;                                 \
 }                                      \
-case Forge::DType::int16: {            \
+case Forge::Dtype::int16: {            \
 using scalar_t = std::int16_t;         \
 __VA_ARGS__();                         \
 break;                                 \
 }                                      \
-case Forge::DType::int32: {            \
+case Forge::Dtype::int32: {            \
 using scalar_t = std::int32_t;         \
 __VA_ARGS__();                         \
 break;                                 \
 }                                      \
-case Forge::DType::int64: {            \
+case Forge::Dtype::int64: {            \
 using scalar_t = std::int64_t;         \
 __VA_ARGS__();                         \
 break;                                 \
@@ -52,11 +52,17 @@ namespace Forge {
 template <typename T>
 concept KernelDerived = std::is_base_of_v<Forge::Kernel, T>;
 
+template <typename T>
+concept Enum = std::is_enum_v<T>;
+
 class Forge::Dispatcher {
 public:
     enum class Ops{add, sub, matmul, OpsCount};
     enum class DispatchKey {CPU, CUDA, CPU_Autodiff, CUDA_Autodiff, DispatchKeysCount};
-    Forge::Kernel* m_registry [Ops::OpsCount][DispatchKey::DispatchKeysCount] {nullptr};
+private:
+    Forge::Kernel* m_registry [static_cast<int>(Ops::OpsCount)][static_cast<int>(DispatchKey::DispatchKeysCount)] {nullptr};
+    template <Enum T>
+    static constexpr int to_idx(T x) {return static_cast<int>(x);}
 public:
     template <KernelDerived T>
     void register_kernel(T* func_class, Ops op, DispatchKey dispatch_key);
@@ -66,14 +72,14 @@ public:
 
 template <KernelDerived T>
 inline void Forge::Dispatcher::register_kernel(T* func_class, Ops op, DispatchKey dispatch_key) {
-    if (Kernel*& kernel {m_registry[static_cast<int>(op)][static_cast<int>(dispatch_key)]}; kernel == nullptr)
+    if (Kernel*& kernel {m_registry[to_idx(op)][to_idx(dispatch_key)]}; kernel == nullptr)
        kernel = func_class;
     else throw std::invalid_argument("Kernel already registered");
 }
 
 template<KernelDerived T>
 inline T* Forge::Dispatcher::lookup(Ops op, DispatchKey dispatch_key) {
-    Kernel* kernel {m_registry[static_cast<int>(op)][static_cast<int>(dispatch_key)]};
+    Kernel* kernel {m_registry[to_idx(op)][to_idx(dispatch_key)]};
     if (kernel==nullptr)
         throw std::invalid_argument("Kernel not registered");
 
