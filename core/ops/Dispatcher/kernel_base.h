9fff259124148df6ea258544f96ff08afd397b6f
muchlakshay <reduxfusion@gmail.com>
2026-02-09 21:32:38 +0530

    Make the destructor virtual in Kernel Class

--- core/ops/Dispatcher/kernel_base.h
+++ core/ops/Dispatcher/kernel_base.h
@@ -10,6 +10,8 @@ namespace Forge {
 class Forge::Kernel {
     ctti::type_id_t m_derived_id{};
 public:
+    virtual ~Kernel() = default;
+
     constexpr explicit Kernel(const ctti::type_id_t& derived_id) : m_derived_id{derived_id} {}
     [[nodiscard]] constexpr ctti::type_id_t derived_id() const {return m_derived_id;}
 };
