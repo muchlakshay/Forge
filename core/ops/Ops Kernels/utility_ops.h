8e425f152b0fb703e0a86566f19855cca9cc75ae
muchlakshay <reduxfusion@gmail.com>
2026-02-12 22:22:59 +0530

    Add constant value kernels with runtime scalar support
    Introduce constant kernels for creating and filling tensors with a uniform value.
    
    -Added Forge::Scalar (std::variant) to represent runtime scalar values across supported numeric types.
    -Introduced ConstantAbstract kernel interface using type-erased scalar input.
    -Implemented ConstantCPU backend to fill tensor memory with a constant value.
    -Integrated runtime dtype resolution via DISPATCH_ALL_TYPES and scalar extraction using std::visit.
    
    This is the initial implementation of constant tensor support for CPU side, CUDA will be implemented later.

--- core/ops/Ops Kernels/utility_ops.h
+++ core/ops/Ops Kernels/utility_ops.h
@@ -6,6 +6,16 @@ namespace Forge {
     void register_utility_kernels(Dispatcher<UtilityOps>& dispatcher);
     struct StorageBackend;
     struct StorageBackendCPU;
+    struct ConstantAbstract;
+    struct ConstantCPU;
+
+    using Scalar = std::variant<
+    float,
+    double,
+    std::int16_t,
+    std::int32_t,
+    std::int64_t
+>;
 }
 
 struct Forge::StorageBackend : public Forge::Kernel {
@@ -25,9 +35,30 @@ private:
     }
 };
 
+struct Forge::ConstantAbstract : public Forge::Kernel {
+  ConstantAbstract() : Kernel{ctti::type_id<ConstantAbstract>()} {}
+  virtual void setConstant(void* mem, std::size_t size, const Scalar& constant, Dtype dtype) const = 0;
+    ~ConstantAbstract() override = default;
+};
+
+struct Forge::ConstantCPU : public ConstantAbstract {
+    void setConstant(void* mem, std::size_t size, const Scalar& constant, Dtype dtype) const override {
+        DISPATCH_ALL_TYPES(dtype, Device::CPU, [&] {
+            auto* ptr {static_cast<scalar_t*>(mem)};
+            std::visit([&](auto value) {
+                using src_t = std::decay_t<decltype(value)>;
+                scalar_t casted {static_cast<scalar_t>(value)};
+                std::fill(ptr, ptr+size, casted);
+            }, constant);
+        });
+    }
+};
+
 inline void Forge::register_utility_kernels(Dispatcher<UtilityOps>& dispatcher) {
     static StorageBackendCPU storageBackendCPU{};
+    static ConstantCPU constantCPU{};
     dispatcher.register_kernel<StorageBackend>(&storageBackendCPU, UtilityOps::storage_backend, DispatchKey::CPU);
+    dispatcher.register_kernel<ConstantAbstract>(&constantCPU, UtilityOps::constant, DispatchKey::CPU);
 }
 
 
