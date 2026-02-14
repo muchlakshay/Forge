55dd54ec1be46563e0f1eb977bbe8d8b14e2854f
muchlakshay <reduxfusion@gmail.com>
2026-02-14 23:14:01 +0530

    Add m_dispatcher data member that will reference to the dispatcher containing all ops and there grad functions
    Refs #15

--- core/autograd/node.h
+++ core/autograd/node.h
@@ -3,15 +3,17 @@
 #include "tensor/tensor.h"
 
 namespace Forge {
-    template <typename OpClass, std::size_t Parents>
+    template <typename OpClass, std::size_t Parents, ValidEnum OpEnum>
     class Node;
 }
-template <typename OpClass, std::size_t Parents>
+template <typename OpClass, std::size_t Parents, ValidEnum OpEnum>
 class Forge::Node final : public NodeAbstract {
     std::array<std::shared_ptr<Tensor>, Parents> m_parents;
     DispatchKey m_dispatch_key {};
+    const Dispatcher<OpClass>& m_dispatcher {};
     public:
-    explicit Node(Device device) : m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff} {}
+    explicit Node(Device device, const Dispatcher<OpEnum>& dispatcher) :
+    m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff}, m_dispatcher{dispatcher} {}
         void backward() const override;
 };
 
