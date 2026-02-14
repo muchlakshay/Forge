8715df52ad433e4a7ecf5ea34728e2f2d28c53c6
muchlakshay <reduxfusion@gmail.com>
2026-02-14 22:59:58 +0530

    Define `Node` class
    - `m_parents` holds all the parent tensor's pointers
    - `m_dispatch_key` (either CPU_Autodiff or CUDA_Autodiff) according to the device
    
    Refs #15

--- core/autograd/node.h
+++ core/autograd/node.h
@@ -6,4 +6,12 @@ namespace Forge {
     template <typename OpClass, std::size_t Parents>
     class Node;
 }
+template <typename OpClass, std::size_t Parents>
+class Forge::Node final : public NodeAbstract {
+    std::array<std::shared_ptr<Tensor>, Parents> m_parents;
+    DispatchKey m_dispatch_key {};
+    public:
+    explicit Node(Device device) : m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff} {}
+        void backward() const override;
+};
 
