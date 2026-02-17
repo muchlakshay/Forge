0600a85318c455edef4e2677bbd184cb352eda99
muchlakshay <reduxfusion@gmail.com>
2026-02-17 17:32:11 +0530

    fix error in attach_node() occurred by directly assigning Node object to a shared_ptr<NodeAbstract> returned by Tensor::node() by first making a local shared_ptr
    Refs #15

--- core/autograd/attach_node.h
+++ core/autograd/attach_node.h
@@ -9,7 +9,9 @@ namespace Forge {
 
 
 template<typename OpClass, std::size_t Parents, ValidEnum OpEnum, typename... ParentTensors>
-void Forge::attach_node(Tensor &tensor, Device device, const Dispatcher<OpEnum> & dispatcher, OpEnum op, ParentTensors &&... parent_tensors) {
-    Node<OpClass, Parents, OpEnum> node {device, dispatcher, op, tensor, std::forward<ParentTensors>(parent_tensors)...};
+void Forge::attach_node(Tensor &tensor, Device device, const Dispatcher<OpEnum> & dispatcher,
+    OpEnum op, ParentTensors &&... parent_tensors) {
+    auto node {std::make_shared<Node<OpClass, Parents, OpEnum>>(device, dispatcher, op, tensor,
+        std::forward<ParentTensors>(parent_tensors)...)};
     tensor.node() = node;
 }
