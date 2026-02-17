dfd3c5990b809c58d81fe7819125b032b9780ee2
muchlakshay <reduxfusion@gmail.com>
2026-02-17 17:30:00 +0530

    initialize m_parents in Node's ctor by copying parent tensors by autodiff context using NodeContext{}
    Refs #15

--- core/autograd/node.h
+++ core/autograd/node.h
@@ -21,13 +21,12 @@ class Forge::Node final : public NodeAbstract {
 public:
     template <typename... ParentTensors>
     explicit Node(Device device, const Dispatcher<OpEnum>& dispatcher,OpEnum op, Tensor& child, ParentTensors&&... parent_tensors) :
-    m_parents{parent_tensors...}, m_child{child}, m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff},
+    m_parents{Forge::Tensor{parent_tensors, Forge::NodeContext{}}...}, m_child{child}, m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff},
     m_dispatcher{dispatcher}, m_op{op}, m_gradFn{dispatcher.template lookup<OpClass>(op, m_dispatch_key)}{
         static_assert(sizeof...(parent_tensors)<=Parents, "Invalid number of parent tensors passed");
     }
         void backward() const override;
 };
-
 
 template<typename OpClass, std::size_t Parents, ValidEnum OpEnum>
 void Forge::Node<OpClass, Parents, OpEnum>::backward() const {
