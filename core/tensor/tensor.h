4811a83bd1e7304ff1a02f2df7ccb7cd0d4d50e4
muchlakshay <reduxfusion@gmail.com>
2026-02-17 13:48:41 +0530

    add grads() member function that returns reference to a static shared_ptr<Tensor> object
    Refs #15

--- core/tensor/tensor.h
+++ core/tensor/tensor.h
@@ -93,6 +93,8 @@ public:
     [[nodiscard]] auto size() const {return m_size;}
     [[nodiscard]] auto dispatch_key() const {return m_dispatch_key;}
     [[nodiscard]] auto& node() const {return m_node;}
+
+    auto& grads() {static std::shared_ptr<Tensor> m_grads; return m_grads;}
 };
 
 template <TensorStorageType T>
