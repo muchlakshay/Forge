71705b354c11b3eda12692f66782385d8660d8ef
muchlakshay <reduxfusion@gmail.com>
2026-02-17 14:23:52 +0530

    change m_parents type from array<shared_ptr<Tensor>, Parents> to array<Tensor, Parents> in Node to copy tensors and store m_child too instead of storing shared_ptr
    Refs #15

--- core/autograd/node.h
+++ core/autograd/node.h
@@ -12,17 +12,18 @@ namespace Forge {
 }
 template <typename OpClass, std::size_t Parents, ValidEnum OpEnum>
 class Forge::Node final : public NodeAbstract {
-    std::array<std::shared_ptr<Tensor>, Parents> m_parents {};
-    std::shared_ptr<Tensor> m_child{};
+    std::array<Tensor, Parents> m_parents {};
+    Tensor m_child{};
     DispatchKey m_dispatch_key {};
     const Dispatcher<OpEnum>& m_dispatcher {};
     OpEnum m_op {};
 public:
     template <typename... ParentTensors>
-    explicit Node(Device device, const Dispatcher<OpEnum>& dispatcher,OpEnum op, ParentTensors&&... parent_tensors) :
-    m_parents{parent_tensors...}, m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff},
+    explicit Node(Device device, const Dispatcher<OpEnum>& dispatcher,OpEnum op, Tensor& child, ParentTensors&&... parent_tensors) :
+    m_parents{parent_tensors...}, m_child{child}, m_dispatch_key{device==Device::CPU?DispatchKey::CPU_Autodiff:DispatchKey::CUDA_Autodiff},
     m_dispatcher{dispatcher}, m_op{op}{
         static_assert(sizeof...(parent_tensors)<=Parents, "Invalid number of parent tensors passed");
     }
         void backward() const override;
 };
+
