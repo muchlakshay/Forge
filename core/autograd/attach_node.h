0f5df30f7e27cd6d13bf840ae8c86f7fec87af2f
muchlakshay <reduxfusion@gmail.com>
2026-02-17 15:52:45 +0530

    implement attach_node()
    this function will create a Node object and assign it to a tensor's m_node, it will be used in math operations kernels to build autodiff graph
    
    Refs #15

--- core/autograd/attach_node.h
+++ core/autograd/attach_node.h
@@ -6,3 +6,10 @@ namespace Forge {
     template <typename OpClass, std::size_t Parents, ValidEnum OpEnum, typename... ParentTensors>
     void attach_node(Tensor& tensor, Device device, const Dispatcher<OpEnum>& dispatcher, OpEnum op, ParentTensors&&... parent_tensors);
 }
+
+
+template<typename OpClass, std::size_t Parents, ValidEnum OpEnum, typename... ParentTensors>
+void Forge::attach_node(Tensor &tensor, Device device, const Dispatcher<OpEnum> & dispatcher, OpEnum op, ParentTensors &&... parent_tensors) {
+    Node<OpClass, Parents, OpEnum> node {device, dispatcher, op, tensor, std::forward<ParentTensors>(parent_tensors)...};
+    tensor.node() = node;
+}
