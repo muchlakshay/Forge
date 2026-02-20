2fa27b0bd3e8980b6c123d4b3b65d26e3c296caf
muchlakshay <reduxfusion@gmail.com>
2026-02-20 22:56:54 +0530

    add storage_copy enumerator in UtilityOps enum

--- core/enums.h
+++ core/enums.h
@@ -4,5 +4,5 @@ namespace Forge {
     enum class Dtype {float32, float16, float64, int16, int32, int64, DtypeCount};
     enum class Device {CPU, CUDA};
     enum class DispatchKey {CPU, CUDA, CPU_Autodiff, CUDA_Autodiff, DispatchKeysCount};
-    enum class UtilityOps {storage_backend, initializers, constant, print, Count};
+    enum class UtilityOps {storage_backend, initializers, constant, print, storage_copy, Count};
 }
