80b2f80b0b65ed0c73384a3532d9a56cac31b969
muchlakshay <reduxfusion@gmail.com>
2026-03-02 23:14:54 +0530

    take tensor as argument instead of storage shared_ptr and rewrite `fill` lambda
    Refs #17

--- primitives/weights_init.h
+++ primitives/weights_init.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <random>
-#include "primitive_dispatcher.h"
-#include "Storage/storage_abstract.h"
+#include "ops/Dispatcher/kernel_base.h"
+#include "tensor.h"
 
 namespace Forge {
     enum class Initializers{xavier_normal, xavier_uniform, he_normal, he_uniform};
@@ -11,21 +11,25 @@ namespace Forge {
 
 struct Forge::WeightsInitAbstract : Kernel {
     WeightsInitAbstract() : Kernel{ctti::type_id<WeightsInitAbstract>()} {}
-    virtual void initialize(std::shared_ptr<StorageAbstract>& weights, std::size_t input_size, std::size_t output_size,
+    virtual void initialize(Tensor& weights, std::size_t input_size, std::size_t output_size,
         Initializers initializer) const = 0;
 };
 
 struct Forge::WeightsInitCPU : WeightsInitAbstract {
-    void initialize(std::shared_ptr<StorageAbstract>& weights, std::size_t input_size, std::size_t output_size,
+    void initialize(Tensor& weights, std::size_t input_size, std::size_t output_size,
         Initializers initializer) const override {
         std::random_device device;
         std::mt19937_64 mt{ device() };
 
         auto fill {
             [&](auto& storage, auto& dist) {
-                for (std::size_t i {}; i<storage->size(); ++i) storage[i] = dist(mt);
-            }
+                DISPATCH_ALL_TYPES(weights.dtype(), Device::CPU,
+                    [&] {
+                        auto* casted_ptr {static_cast<scalar_t*>(weights.data())};
+                        for (std::size_t i {}; i<weights.size(); ++i)
+                            casted_ptr[i] = static_cast<scalar_t>(dist(mt));
+                    });}
         };
-
     }
 };
+
