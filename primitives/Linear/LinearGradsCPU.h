09c3e9620eb908fdedf75879b661ed2725d735a7
muchlakshay <reduxfusion@gmail.com>
2026-03-17 23:10:14 +0530

    implement grads calculation for input tensor
    d_loss/d_input = d_loss/d_output * weights
    (x, y, batch_size, input_size) = (x, y, batch_size, output_size) * (1, 1, output_size, input_size)
    
    perform contraction over 3rd dim of output_grads_map and 2nd dim of weights
    
    Refs #17

--- primitives/Linear/LinearGradsCPU.h
+++ primitives/Linear/LinearGradsCPU.h
@@ -7,6 +7,18 @@ namespace Forge {
 
 struct Forge::LinearGradsCPU final :  LinearGradsAbstract {
     void compute_grads(Tensor& input, Tensor& weights, Tensor& bias, const Tensor& output) const override {
+        using grads_t = float;
+        const auto tensor_dtype {input.dtype()};
+        Eigen::array<Eigen::IndexPair<std::size_t>, 1> contraction_dims{};
 
+        auto output_grads_map {output.gradients().as_eigen<grads_t>()};
+        DISPATCH_ALL_TYPES(tensor_dtype, Device::CPU, [&] {
+            if (input.need_grads()) {
+                auto input_grads_map {input.gradients().as_eigen<grads_t>()};
+                contraction_dims[0] = Eigen::IndexPair<std::size_t>(3, 2);
+                input_grads_map += output_grads_map.contract(weights.as_eigen<scalar_t>(),
+                    contraction_dims).reshape(input_grads_map.dimensions());
+            }
+        });
     }
 };
