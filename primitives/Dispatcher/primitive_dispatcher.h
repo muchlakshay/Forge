4866639c4d9cbb28333d32c111d19fac6af89f5a
muchlakshay <reduxfusion@gmail.com>
2026-03-20 22:24:32 +0530

    remove `linear_grads` from primitive_ops Enum
    Refs #17

--- primitives/Dispatcher/primitive_dispatcher.h
+++ primitives/Dispatcher/primitive_dispatcher.h
@@ -2,7 +2,7 @@
 #include "ops/Dispatcher/dispatcher.h"
 
 namespace Forge{
-    enum class primitive_ops{linear, linear_grads, weightsInit, Count};
+    enum class primitive_ops{linear, weightsInit, Count};
 
     inline Dispatcher<primitive_ops> & primitive_dispatcher() {
         static Dispatcher<primitive_ops> dispatcher;
