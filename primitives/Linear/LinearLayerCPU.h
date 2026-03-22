c3d83589e7c23eef13c28c23249ae83f02c83cc9
muchlakshay <reduxfusion@gmail.com>
2026-03-22 19:48:26 +0530

    add assert to check correct dims of bias tensor
    bias tensor must be of shape (1, 1, 1, output_size)
    
    Refs #17

--- primitives/Linear/LinearLayerCPU.h
+++ primitives/Linear/LinearLayerCPU.h
@@ -18,12 +18,17 @@ struct Forge::LinearCPU final : LinearAbstract {
             auto transposed_weights {weights_map.shuffle(shuffling)};
 
             Eigen::array<Eigen::IndexPair<std::size_t>, 1> contraction_dims {Eigen::IndexPair<std::size_t>(3, 2)};
+
             output_map = input_map.contract(transposed_weights,
                 contraction_dims).reshape(output_map.dimensions());
 
             if (using_bias) {
                 auto bias_map    { bias.as_eigen<scalar_t>() };
                 auto opt_dims {output_map.dimensions()};
+
+                assert(bias_map.dimension(0) == 1 && bias_map.dimension(1) == 1 &&
+                    bias_map.dimension(2) == 1 && "bias must be (1,1,1,output_size)");
+
                 Eigen::array<Eigen::Index , 4> broadcast_dims {opt_dims[0], opt_dims[1], opt_dims[2], 1};
                 output_map+=bias_map.broadcast(broadcast_dims);
             }
