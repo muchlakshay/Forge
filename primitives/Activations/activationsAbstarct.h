382e0d953ee92748ffdb68b4d95dd81ef1265bcc
muchlakshay <reduxfusion@gmail.com>
2026-03-24 23:06:23 +0530

    make output parameter reference to non-const
    Refs #20

--- primitives/Activations/activationsAbstarct.h
+++ primitives/Activations/activationsAbstarct.h
@@ -13,25 +13,25 @@ namespace Forge {
 
 struct Forge::ReluAbstract : Kernel{
     ReluAbstract() : Kernel{ctti::type_id<ReluAbstract>()}{}
-    virtual void forward(const Tensor& input, const Tensor& output) = 0;
+    virtual void forward(const Tensor& input, Tensor& output) = 0;
 };
 
 struct Forge::SigmoidAbstract : Kernel{
     SigmoidAbstract() : Kernel{ctti::type_id<SigmoidAbstract>()}{}
-    virtual void forward(const Tensor& input, const Tensor& output) = 0;
+    virtual void forward(const Tensor& input, Tensor& output) = 0;
 };
 
 struct Forge::SoftmaxAbstract : Kernel{
     SoftmaxAbstract() : Kernel{ctti::type_id<SoftmaxAbstract>()}{}
-    virtual void forward(const Tensor& input, const Tensor& output) = 0;
+    virtual void forward(const Tensor& input, Tensor& output) = 0;
 };
 
 struct Forge::TanhAbstract : Kernel{
     TanhAbstract() : Kernel{ctti::type_id<TanhAbstract>()}{}
-    virtual void forward(const Tensor& input, const Tensor& output) = 0;
+    virtual void forward(const Tensor& input, Tensor& output) = 0;
 };
 
 struct Forge::LeakyReluAbstract : Kernel{
     LeakyReluAbstract() : Kernel{ctti::type_id<LeakyReluAbstract>()}{}
-    virtual void forward(const Tensor& input, const Tensor& output) = 0;
+    virtual void forward(const Tensor& input, Tensor& output) = 0;
 };
