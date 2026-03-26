30f41af8c7b69307379ecfd713880bfd6a9b1be3
muchlakshay <reduxfusion@gmail.com>
2026-03-27 00:26:53 +0530

    make every activation struct attach a node in the output tensor, if input need grads
    Refs #20

--- primitives/Activations/activations.h
+++ primitives/Activations/activations.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "tensor.h"
 #include "activationsAbstarct.h"
+#include "acvitvationsGradsAbstract.h"
 #include "../Dispatcher/primitive_dispatcher.h"
+#include "autograd/attach_node.h"
 
 namespace Forge {
     struct Relu;
@@ -17,6 +19,9 @@ struct Forge::Relu {
         Tensor output {input.shape(), input.dtype(), input.need_grads(), input.device()};
         const auto* relu {primitive_dispatcher().lookup<ReluAbstract>(primitive_ops::relu, input.dispatch_key())};
         relu->forward(input, output);
+        if (output.need_grads()) {
+            attach_node<ReluGradsAbstract, 1>(output, output.device(), primitive_dispatcher(), primitive_ops::relu, input);
+        }
         return output;
     }
 };
@@ -26,6 +31,9 @@ struct Forge::Softmax {
         Tensor output {input.shape(), input.dtype(), input.need_grads(), input.device()};
         const auto* softmax {primitive_dispatcher().lookup<SoftmaxAbstract>(primitive_ops::softmax, input.dispatch_key())};
         softmax->forward(input, output);
+        if (output.need_grads()) {
+            attach_node<SoftmaxGradsAbstract, 1>(output, output.device(), primitive_dispatcher(), primitive_ops::softmax, input);
+        }
         return output;
     }
 };
@@ -35,6 +43,9 @@ struct Forge::LeakyRelu {
         Tensor output {input.shape(), input.dtype(), input.need_grads(), input.device()};
         const auto* leaky_relu {primitive_dispatcher().lookup<LeakyReluAbstract>(primitive_ops::leaky_relu, input.dispatch_key())};
         leaky_relu->forward(input, output);
+        if (output.need_grads()) {
+            attach_node<LeakyReluGradsAbstract, 1>(output, output.device(), primitive_dispatcher(), primitive_ops::leaky_relu, input);
+        }
         return output;
     }
 };
@@ -44,6 +55,9 @@ struct Forge::Tanh {
         Tensor output {input.shape(), input.dtype(), input.need_grads(), input.device()};
         const auto* tanh {primitive_dispatcher().lookup<TanhAbstract>(primitive_ops::tanh, input.dispatch_key())};
         tanh->forward(input, output);
+        if (output.need_grads()) {
+            attach_node<TanhGradsAbstract, 1>(output, output.device(), primitive_dispatcher(), primitive_ops::tanh, input);
+        }
         return output;
     }
 };
@@ -53,6 +67,9 @@ struct Forge::Gelu {
         Tensor output {input.shape(), input.dtype(), input.need_grads(), input.device()};
         const auto* gelu {primitive_dispatcher().lookup<GeluAbstract>(primitive_ops::gelu, input.dispatch_key())};
         gelu->forward(input, output);
+        if (output.need_grads()) {
+            attach_node<GeluGradsAbstract, 1>(output, output.device(), primitive_dispatcher(), primitive_ops::gelu, input);
+        }
         return output;
     }
 };
@@ -62,6 +79,9 @@ struct Forge::Sigmoid {
         Tensor output {input.shape(), input.dtype(), input.need_grads(), input.device()};
         const auto* sigmoid {primitive_dispatcher().lookup<SigmoidAbstract>(primitive_ops::sigmoid, input.dispatch_key())};
         sigmoid->forward(input, output);
+        if (output.need_grads()) {
+            attach_node<SigmoidGradsAbstract, 1>(output, output.device(), primitive_dispatcher(), primitive_ops::sigmoid, input);
+        }
         return output;
     }
 };
