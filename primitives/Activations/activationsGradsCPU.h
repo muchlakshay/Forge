63b16dbf4071c381e1631dcc39585d4a0ee69699
muchlakshay <reduxfusion@gmail.com>
2026-03-26 23:17:31 +0530

    implement leaky-relu's grads function (`LeakyReluGradsCPU`)
    Refs #20

--- primitives/Activations/activationsGradsCPU.h
+++ primitives/Activations/activationsGradsCPU.h
@@ -22,4 +22,15 @@ struct Forge::ReluGradsCPU : ReluGradsAbstract {
     }
 };
 
+struct Forge::LeakyReluGradsCPU : LeakyReluGradsAbstract {
+  void compute_grads(const Tensor& preactivations, const Tensor& activations) const override {
+      DISPATCH_ALL_TYPES(preactivations.dtype(), Device::CPU, [&] {
+          auto preact_map {preactivations.as_eigen<scalar_t>()};
+          auto preact_grads_map {preactivations.gradients().as_eigen<scalar_t>()};
+          auto act_grads_map {activations.gradients().as_eigen<scalar_t>()};
+          const auto alpha {static_cast<scalar_t>(0.01)}, zero{static_cast<scalar_t>(0)}, one {static_cast<scalar_t>(1)};
+          preact_grads_map += act_grads_map*(preact_map>zero).select(one, alpha);
+      });
+  }
+};
 
