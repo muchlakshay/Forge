4899a8f8784e0b8b3b3f740b82c909cfab9c5702
muchlakshay <reduxfusion@gmail.com>
2026-04-05 21:51:12 +0530

    change return type of `operator()` member to const Tensor&
    Refs #24

--- primitives/Loss Functions/lossFunctionsBase.h
+++ primitives/Loss Functions/lossFunctionsBase.h
@@ -6,7 +6,7 @@ namespace Forge {
 }
 
 struct Forge::LossFunction {
-    virtual Tensor operator()(const Tensor& predictions, const Tensor& ground_truth)= 0;
+    virtual const Tensor& operator()(const Tensor& predictions, const Tensor& ground_truth)= 0;
     virtual void backward() const = 0;
     virtual ~LossFunction() = default;
 };
