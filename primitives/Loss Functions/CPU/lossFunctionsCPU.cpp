9ba81b5b96fc843cc83f633f38105ae49b6ebc62
muchlakshay <reduxfusion@gmail.com>
2026-04-05 21:40:10 +0530

    fix static assertion triggered in MSE code
    when compiling, the code triggered some cryptic static assertion, during debugging i rearraged code a bit to break up statements to figure out what was causing the error, it was found that the assertion was related to layout of tensors, so i explicitly mentioned the row-major order for the sum_val scalar tensor and it resolved
    
    Refs #24

--- primitives/Loss Functions/CPU/lossFunctionsCPU.cpp
+++ primitives/Loss Functions/CPU/lossFunctionsCPU.cpp
@@ -1,12 +1,14 @@
 #include "lossFunctionsCPU.h"
+#include <iostream>
 
 void Forge::MSE_CPU::compute_loss(const Tensor &pred, const Tensor &ground_truth, Tensor &loss) const {
-    auto loss_map {loss.as_eigen<float>()};
     DISPATCH_ALL_TYPES(pred.dtype(), Device::CPU, [&] {
-        auto pred_map {pred.as_eigen<scalar_t>()};
-        auto ground_truth_map {ground_truth.as_eigen<scalar_t>()};
-        loss_map = ((ground_truth_map-pred_map).square().sum()/pred_map.size()).template cast<float>;
-    });
+        auto pred_map_f          {pred.as_eigen<scalar_t>()};
+        auto ground_truth_map_f  {ground_truth.as_eigen<scalar_t>()};
+
+        Eigen::Tensor<scalar_t, 0, Eigen::RowMajor> sum_val { (ground_truth_map_f - pred_map_f).square().sum()};
+        loss.as_eigen<float>()(0, 0, 0, 0) = (static_cast<float>(sum_val())/static_cast<float>(pred.size()));
+        });
 }
 
 
