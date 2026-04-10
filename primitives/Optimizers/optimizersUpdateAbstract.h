4f6501d05647f4b7cb333280f163357d68b233db
muchlakshay <reduxfusion@gmail.com>
2026-04-10 21:56:29 +0530

    add base class for device specific update implementation for SGD
    Refs #26

---
+++ primitives/Optimizers/optimizersUpdateAbstract.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ops/Dispatcher/kernel_base.h"
+#include "tensor.h"
+#include <vector>
+
+namespace Forge{
+    struct SGDUpdateAbstract;
+}
+
+struct Forge::SGDUpdateAbstract : Kernel {
+    SGDUpdateAbstract() : Kernel{ctti::type_id<SGDUpdateAbstract>()} {}
+    virtual void update(const std::vector<Tensor*>& params, float lr) const = 0;
+};
