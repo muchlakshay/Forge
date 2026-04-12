e41d68cf547cde56044c3f059b0c5884a75f2f17
muchlakshay <reduxfusion@gmail.com>
2026-04-12 14:59:35 +0530

    add `m_momentum_coef` data member and `momentum_coef` parameter in ctor
    Refs #26

--- primitives/Optimizers/optimizers.h
+++ primitives/Optimizers/optimizers.h
@@ -8,9 +8,11 @@ namespace Forge {
 
 class Forge::SGD {
     std::vector<Tensor*> m_parameters{};
+    std::vector<Tensor> m_V;
     float m_learningRate {};
+    float m_momentum_coef {};
 public:
-    explicit SGD(std::vector<Tensor *> parameters, const float lr) : m_parameters{std::move(parameters)}, m_learningRate {lr}{}
+    explicit SGD(std::vector<Tensor *> parameters, float lr=0.01f, float momentum_coef=0.0f);
     void update() const;
     void clear_grads() const {for (const auto p : m_parameters) p->clear_grads();};
     [[nodiscard]] auto parameters() const {return m_parameters;}
