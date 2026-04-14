541684244bf1aa938bf96447de61c3e30a4ce1a9
muchlakshay <reduxfusion@gmail.com>
2026-04-14 17:27:46 +0530

    add a getter and some setters in `Forge::SGD`
    Refs #26

--- primitives/Optimizers/optimizers.h
+++ primitives/Optimizers/optimizers.h
@@ -4,6 +4,7 @@
 
 namespace Forge {
     class SGD;
+    class Adam;
 }
 
 class Forge::SGD {
@@ -15,6 +16,9 @@ public:
     explicit SGD(std::vector<Tensor *> parameters, float lr=0.01f, float momentum_coef=0.0f);
     void update() const;
     void clear_grads() const {for (const auto p : m_parameters) p->clear_grads();};
-    [[nodiscard]] auto parameters() const {return m_parameters;}
+    void setLearningRate(const float lr) {m_learningRate = lr;}
+    void setMomentumCoef(const float momentum_coef) {m_momentum_coef = momentum_coef;}
+    [[nodiscard]] const auto& parameters() const {return m_parameters;}
     [[nodiscard]] auto learningRate() const {return m_learningRate;}
+    [[nodiscard]] auto momentumCoef() const {return m_momentum_coef;}
 };
