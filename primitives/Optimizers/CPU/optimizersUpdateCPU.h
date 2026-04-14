7892086ea3dbff305c8ab877997f70f66a6bf1a6
muchlakshay <reduxfusion@gmail.com>
2026-04-14 17:34:19 +0530

    add `Forge::AdamUpdateCPU` for CPU update logic implementation of Adam
    Refs #26

--- primitives/Optimizers/CPU/optimizersUpdateCPU.h
+++ primitives/Optimizers/CPU/optimizersUpdateCPU.h
@@ -3,7 +3,13 @@
 
 namespace Forge {
     struct SGDUpdateCPU;
+    struct AdamUpdateCPU;
 }
 
 struct Forge::SGDUpdateCPU : SGDUpdateAbstract {void update(const std::vector<Tensor*>& params, float lr,
     float momentum_coef, const std::vector<Tensor>& V) const override;};
+
+struct Forge::AdamUpdateCPU : AdamUpdateAbstract {
+    void update(const std::vector<Tensor *> &params, float lr, const std::vector<Tensor> &V,
+        const std::vector<Tensor> &M, float beta_1, float beta_2, int epoch) const override;
+};
