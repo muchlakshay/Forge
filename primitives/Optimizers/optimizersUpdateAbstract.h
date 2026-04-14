545e3bf0cb57ee53029672f22b3c9b26ebfd99d4
muchlakshay <reduxfusion@gmail.com>
2026-04-14 17:33:11 +0530

    add `Forge::AdamUpdateAbstract` struct for device specific update logic implementations
    Refs #26

--- primitives/Optimizers/optimizersUpdateAbstract.h
+++ primitives/Optimizers/optimizersUpdateAbstract.h
@@ -5,10 +5,17 @@
 
 namespace Forge{
     struct SGDUpdateAbstract;
+    struct AdamUpdateAbstract;
 }
 
 struct Forge::SGDUpdateAbstract : Kernel {
     SGDUpdateAbstract() : Kernel{ctti::type_id<SGDUpdateAbstract>()} {}
     virtual void update(const std::vector<Tensor*>& params, float lr, float momentum_coef,
         const std::vector<Tensor>& V) const = 0;
+};
+
+struct Forge::AdamUpdateAbstract : Kernel {
+    AdamUpdateAbstract() : Kernel{ctti::type_id<AdamUpdateAbstract>()} {}
+    virtual void update(const std::vector<Tensor*>& params, float lr, const std::vector<Tensor>& V,
+        const std::vector<Tensor>& M, float beta_1, float beta_2, int epoch) const = 0;
 };
