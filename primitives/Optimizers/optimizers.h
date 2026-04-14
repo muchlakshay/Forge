17557bb410f76c311c4b2bc2a3c8169284c197cd
muchlakshay <reduxfusion@gmail.com>
2026-04-14 17:31:41 +0530

    define `Forge::Adam` class
    add all the state data members, setters and getters
    
    Refs #26

--- primitives/Optimizers/optimizers.h
+++ primitives/Optimizers/optimizers.h
@@ -22,3 +22,27 @@ public:
     [[nodiscard]] auto learningRate() const {return m_learningRate;}
     [[nodiscard]] auto momentumCoef() const {return m_momentum_coef;}
 };
+
+class Forge::Adam {
+    std::vector<Tensor*> m_parameters{};
+    std::vector<Tensor> m_V{};
+    std::vector<Tensor> m_M{};
+    float m_learningRate {};
+    mutable int m_epoch{1};
+    float m_beta_1 {}, m_beta_2 {};
+public:
+    explicit  Adam(const std::vector<Tensor*>& parameters, float lr=0.01f, float beta_1=0.9f, float beta_2=0.999f);
+    void clear_grads() const {for (const auto p : m_parameters) p->clear_grads();}
+    void reset() const {m_epoch = 1;}
+    void update() const;
+    void setLearningRate(const float lr) {m_learningRate = lr;}
+    void setBeta_1(const float beta_1) {m_beta_1 = beta_1;}
+    void setBeta_2(const float beta_2) {m_beta_2 = beta_2;}
+    [[nodiscard]] const auto& parameters() const {return m_parameters;}
+    [[nodiscard]] const auto& firstMoment() const {return m_M;}
+    [[nodiscard]] const auto& secondMoment() const {return m_M;}
+    [[nodiscard]] auto beta_1() const {return m_beta_1;}
+    [[nodiscard]] auto beta_2() const {return m_beta_2;}
+    [[nodiscard]] auto learningRate() const {return m_learningRate;}
+    [[nodiscard]] auto epoch() const {return m_epoch;}
+};
