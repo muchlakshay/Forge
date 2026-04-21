5302dbf36169f3eaa105c21021d1f246b73d22e6
muchlakshay <reduxfusion@gmail.com>
2026-04-21 21:58:53 +0530

    add a linear layer data member in `Forge::Linear`
    Refs #28

--- primitives/Attention/Self Attention/self_attention.h
+++ primitives/Attention/Self Attention/self_attention.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstddef>
 #include "tensor.h"
+#include "Linear/LinearLayer.h"
 #include "Weights Init/weights_init.h"
 
 namespace Forge {
@@ -11,12 +12,14 @@ class Forge::SelfAttention {
     Tensor m_query_W {};
     Tensor m_key_W {};
     Tensor m_value_W {};
+    Linear m_linear;
     bool m_mask {};
     DispatchKey m_dispatch_key {};
+    std::size_t m_heads{};
 public:
     SelfAttention(std::size_t seq_len, std::size_t Q_K_dims, std::size_t V_dims, std::size_t heads, bool mask,
         Device device, Dtype=Dtype::float32, Initializers initializer=Initializers::he_normal);
-    Tensor operator()(const Tensor& input);
+    Tensor operator()(const Tensor& input) const;
     [[nodiscard]] auto query() const {return m_query_W;}
     [[nodiscard]] auto key() const {return m_key_W;}
     [[nodiscard]] auto value() const {return m_value_W;}
--- primitives/Attention/Self Attention/self_attention_impl_abstract.h
+++ primitives/Attention/Self Attention/self_attention_impl_abstract.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "tensor.h"
 #include "ops/Dispatcher/kernel_base.h"
+#include "Linear/LinearLayer.h"
 
 namespace Forge {
     struct SelfAttentionImplAbstract;
@@ -8,5 +9,6 @@ namespace Forge {
 
 struct Forge::SelfAttentionImplAbstract : Kernel{
     SelfAttentionImplAbstract() : Kernel{ctti::type_id<SelfAttentionImplAbstract>()} {}
-    virtual void forward(Tensor input, Tensor query_W, Tensor key_W, Tensor value_W, Tensor output, bool mask) const = 0;
+    virtual void forward(Tensor input, Tensor query_W, Tensor key_W, Tensor value_W, Tensor output, Linear& linear,
+        std::size_t heads, bool mask) const = 0;
 };
