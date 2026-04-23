3fa986303d1d80c1a79ecde5ec3839a7b85ee64e
muchlakshay <reduxfusion@gmail.com>
2026-04-23 21:56:30 +0530

    add `Forge::SelfAttentionCPU` for cpu SA implementation
    Refs #28

---
+++ primitives/Attention/Self Attention/CPU/self_attention_cpu.h
@@ -0,0 +1,10 @@
+#include "Attention/Self Attention/self_attention_impl_abstract.h"
+
+namespace Forge {
+    struct SelfAttentionCPU;
+}
+
+struct Forge::SelfAttentionCPU : SelfAttentionImplAbstract {
+    void forward(Tensor input, Tensor query_W, Tensor key_W, Tensor value_W, Tensor output, Linear& linear,
+        std::size_t heads, bool mask) const override;
+};
