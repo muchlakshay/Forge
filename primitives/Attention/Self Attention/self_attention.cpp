977552969a8766c0973a4346697f9ae178f6602f
Lakshya Sharma <reduxfusion@gmail.com>
2026-05-06 22:45:49 +0530

    fix semantic error where Q_w, K_w and V_w should be of dim (heads, d_model, Q_K_dims or V_dims) not (heads, seq_len, Q_K_dims or V_dims)

--- primitives/Attention/Self Attention/self_attention.cpp
+++ primitives/Attention/Self Attention/self_attention.cpp
@@ -5,10 +5,10 @@
 #include "Weights Init/weights_init.h"
 #include <limits>
 
-Forge::SelfAttention::SelfAttention(std::size_t seq_len, std::size_t Q_K_dims, std::size_t V_dims, std::size_t heads,
-    bool mask, Device device, Dtype dtype, Initializers initializer) : m_query_W {{heads, seq_len, Q_K_dims}, dtype, true, device},
-    m_key_W{{heads, seq_len, Q_K_dims}, dtype, true, device}, m_value_W {{heads, seq_len, V_dims}, dtype, true, device},
-    m_linear{V_dims * heads, seq_len, Initializers::xavier_normal, m_query_W.dtype(), m_query_W.device(), false},
+Forge::SelfAttention::SelfAttention(std::size_t d_model, std::size_t seq_len, std::size_t Q_K_dims, std::size_t V_dims, std::size_t heads,
+    bool mask, Device device, Dtype dtype, Initializers initializer) : m_query_W {{heads, d_model, Q_K_dims}, dtype, true, device},
+    m_key_W{{heads, d_model, Q_K_dims}, dtype, true, device}, m_value_W {{heads, d_model, V_dims}, dtype, true, device},
+    m_linear{V_dims * heads, d_model, Initializers::xavier_normal, m_query_W.dtype(), m_query_W.device(), false},
     m_mask{mask}, m_dispatch_key{device==Device::CPU?DispatchKey::CPU:DispatchKey::CUDA}, m_heads{heads}{
 
     const auto* init {primitive_dispatcher().lookup<WeightsInitAbstract>(primitive_ops::weightsInit, m_dispatch_key)};
