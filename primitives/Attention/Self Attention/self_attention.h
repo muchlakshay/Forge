76c7760857faf43e8bc4462ed6fa480cb9ca6780
muchlakshay <reduxfusion@gmail.com>
2026-05-14 21:44:24 +0530

    add new getters in `Forge::SelfAttention`

--- primitives/Attention/Self Attention/self_attention.h
+++ primitives/Attention/Self Attention/self_attention.h
@@ -25,10 +25,16 @@ public:
         Device device, Dtype=Dtype::float32, Initializers initializer=Initializers::he_normal);
     Tensor operator()(const Tensor& input) const;
     void createMask(std::size_t seq_len);
-    auto mask() const {return m_mask_;}
-    [[nodiscard]] auto query() const {return m_query_W;}
-    [[nodiscard]] auto key() const {return m_key_W;}
-    [[nodiscard]] auto value() const {return m_value_W;}
+    auto& mask() const {return m_mask_;}
+    auto& linear() {return m_linear;}
+    [[nodiscard]] auto& query() const {return m_query_W;}
+    [[nodiscard]] auto& key() const {return m_key_W;}
+    [[nodiscard]] auto& value() const {return m_value_W;}
     [[nodiscard]] auto using_mask() const {return m_mask;}
     [[nodiscard]] auto dispatch_key() const {return m_dispatch_key;}
+    [[nodiscard]] auto device() const {return m_device;}
+    [[nodiscard]] auto dtype() const {return m_dtype;}
+    [[nodiscard]] auto heads() const {return m_heads;}
+    [[nodiscard]] auto d_model() const {return m_d_model;}
+    [[nodiscard]] auto useMask() const {return m_mask;}
 };
