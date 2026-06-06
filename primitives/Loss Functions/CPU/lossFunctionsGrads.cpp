e0dd6d520771e82bc8999f8806773bf059faa98d
muchlakshay <reduxfusion@gmail.com>
2026-06-06 22:53:50 +0530

    add grads calculation logic in cross entropy's loss grads fn if ground truths were ids

--- primitives/Loss Functions/CPU/lossFunctionsGrads.cpp
+++ primitives/Loss Functions/CPU/lossFunctionsGrads.cpp
@@ -19,25 +19,49 @@ void Forge::MSEGradsCPU::compute_grads(const Tensor &pred, const Tensor &ground_
 
 void Forge::CrossEntropyGradsCPU::compute_grads(const Tensor &pred, const Tensor &ground_truth, const Tensor &loss) const {
     using grads_t = float;
+    Tensor softmax_eval {{pred.shape()}, Dtype::float32, false, pred.device()};
+    Tensor row_max_eval {{pred.shape()}, Dtype::float32, false, pred.device()};
+
     DISPATCH_ALL_TYPES(pred.dtype(), Device::CPU, [&] {
-    auto pred_map{pred.as_eigen<scalar_t>()};
-    auto pred_map_f {pred_map.template cast<grads_t>()};
-    auto gt_map_f {ground_truth.as_eigen<scalar_t>().template cast<grads_t>()};
-    auto pred_grads_map{pred.gradients().as_eigen<grads_t>()};
-     auto loss_grads_val {loss.gradients().as_eigen<grads_t>()(0, 0, 0, 0)};
 
-    auto& dims {pred_map.dimensions()};
-    Eigen::array<Eigen::Index, 1> class_dim {3};
-    Eigen::array<Eigen::Index, 4> keep_dim  {dims[0], dims[1], dims[2], 1};
-    Eigen::array<Eigen::Index, 4> bcast      {1, 1, 1, dims[3]};
+        auto pred_map{pred.as_eigen<scalar_t>()};
+        auto pred_map_f {pred_map.template cast<grads_t>()};
+        auto gt_map_f {ground_truth.as_eigen<scalar_t>().template cast<grads_t>()};
+        auto softmax_eval_map {softmax_eval.as_eigen<grads_t>()};
 
-    auto row_max{pred_map_f.maximum(class_dim).reshape(keep_dim).broadcast(bcast)};
-    auto shifted{pred_map_f - row_max};
-    auto exp_s {shifted.exp()};
-    auto softmax{exp_s / exp_s.sum(class_dim).reshape(keep_dim).broadcast(bcast)};
+        auto pred_grads_map{pred.gradients().as_eigen<grads_t>()};
+        auto loss_grads_val {loss.gradients().as_eigen<grads_t>()(0, 0, 0, 0)};
+        auto row_max_eval_map {row_max_eval.as_eigen<grads_t>()};
 
-    auto N {static_cast<grads_t>(dims[2])};
-    pred_grads_map += (softmax - gt_map_f) / N * loss_grads_val;
+
+        auto& dims {pred_map.dimensions()};
+        Eigen::array<Eigen::Index, 1> class_dim {3};
+        Eigen::array<Eigen::Index, 4> keep_dim  {dims[0], dims[1], dims[2], 1};
+        Eigen::array<Eigen::Index, 4> bcast      {1, 1, 1, dims[3]};
+
+        forge_eval(row_max_eval_map, (pred_map_f.maximum(class_dim).reshape(keep_dim).broadcast(bcast)));
+        auto shifted{pred_map_f - row_max_eval_map};
+        auto exp_s {shifted.exp()};
+        forge_eval(softmax_eval_map, (exp_s / exp_s.sum(class_dim).reshape(keep_dim).broadcast(bcast)));
+
+        auto N {static_cast<grads_t>(dims[0]*dims[1]*dims[2])};
+
+        auto one_by_N {static_cast<grads_t>(1)/N};
+        if (ground_truth.dtype() == Dtype::int32 && ground_truth.shape().size() == pred.shape().size()-1) {
+            auto idx_map {ground_truth.as_eigen<int>()};
+            auto one {static_cast<grads_t>(1.0f)};
+            auto one_by_N {one/N};
+
+            idx_map = idx_map.reshape(Eigen::array<Eigen::Index, 4>{1, 1, 1, idx_map.size()});
+            softmax_eval_map = softmax_eval_map.reshape(
+                Eigen::array<Eigen::Index, 4>{1, 1, dims[0]*dims[1]*dims[2], dims[3]});
+
+            for (std::size_t B {}; B<idx_map.size(); ++B)  softmax_eval_map(0, 0, B, idx_map(0, 0, 0, B)) -= one;
+            forge_eval(pred_grads_map, (pred_grads_map + softmax_eval_map.reshape(pred_map.dimensions()) * one_by_N * loss_grads_val));
+            return;
+        }
+
+        forge_eval(pred_grads_map, (pred_grads_map + (softmax_eval_map - gt_map_f) * one_by_N * loss_grads_val));
     });
 }
 
