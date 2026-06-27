e7c133dd8c5efccdaa61041ea36061bec57045d0
muchlakshay <reduxfusion@gmail.com>
2026-06-27 22:55:42 +0530

    add ExeCTX struct to control threads used for computation by openBLAS and omp
    a global execution ctx is made "forge_exeCTX"
    it can be used to set the number of threads to be used
    
    its a singleton and cant be copied or moved in any way

--- core/ops/Maths/CPU/math_cpu.h
+++ core/ops/Maths/CPU/math_cpu.h
@@ -10,32 +10,28 @@ namespace Forge {
     void forge_contract(const DTYPE* A_ptr, const DTYPE* B_ptr, DTYPE* C, int M, int N, int K, bool transA,
         bool transB, bool rowMajor, DTYPE alpha=1.0,  DTYPE beta=0.0);
 
+    struct ExeCTX {
+        int m_threads{static_cast<int>(std::thread::hardware_concurrency()/2)};
+        explicit ExeCTX(int threads) : m_threads(threads) {
+            openblas_set_num_threads(threads);
+            omp_set_num_threads(threads);
+        }
+        ExeCTX() = default;
+        ExeCTX(const ExeCTX&) = delete;
+        ExeCTX(ExeCTX&&) = delete;
+        ExeCTX& operator=(const ExeCTX&) = delete;
+        ExeCTX& operator=(ExeCTX&&) = delete;
+        void set_num_threads(int threads) {
+            m_threads = threads;
+            openblas_set_num_threads(threads);
+            omp_set_num_threads(threads);
+        }
+        [[nodiscard]] auto get_num_threads() const {return m_threads;}
+    };
+
+    inline ExeCTX forge_exeCTX;
 }
 
-template<typename ReturnType, typename... Args>
-auto Forge::select_optimal_kernel(ReturnType (*AVX2_fn)(Args...), ReturnType (*AVX512_fn)(Args...),
-    ReturnType (*FALLBACK_fn)(Args...)) -> ReturnType(*)(Args...) {
-#if defined (__x86_64__) || defined(_M_X64)
-    #if defined(__GNUC__) || defined(__clang__)
-        if (__builtin_cpu_supports("avx512f")) return AVX512_fn;
-        if (__builtin_cpu_supports("avx2")) return AVX2_fn;
-        return FALLBACK_fn;
-    #elif defined(_MSC_VER)
-        int cpuInfo[4] = {0};
-        __cpuid(cpuInfo, 1);
-        bool osxsave = (cpuInfo[2] & (1 << 27)) != 0;
-        if (osxsave) {
-            __cpuid(cpuInfo, 7);
-            bool has_avx512 = (cpuInfo[1] & (1 << 16)) != 0;
-            bool has_avx2   = (cpuInfo[1] & (1 << 5)) != 0;
-            if (has_avx512) return AVX512_fn;
-            if (has_avx2) return AVX2_fn;
-        }
-        return FALLBACK_fn;
-    #endif
-#endif
-    return FALLBACK_fn;
-}
 
 template<typename DTYPE>
 void Forge::forge_contract(const DTYPE* A_ptr, const DTYPE* B_ptr, DTYPE* C, int M, int N, int K, bool transA,
@@ -64,3 +60,5 @@ void Forge::forge_contract(const DTYPE* A_ptr, const DTYPE* B_ptr, DTYPE* C, int
         throw std::runtime_error("Unsupported DTYPE For Contraction");
     }
 }
+
+
