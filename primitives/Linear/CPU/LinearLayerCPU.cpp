b209b13ce61fcca96dfcc29e797603658ec0f69a
muchlakshay <reduxfusion@gmail.com>
2026-07-10 23:13:14 +0530

    use `forge_contract` for matmul in linear forward pass instead of Eigen

--- primitives/Linear/CPU/LinearLayerCPU.cpp
+++ primitives/Linear/CPU/LinearLayerCPU.cpp
@@ -1,36 +1,48 @@
 #include "LinearLayerCPU.h"
+#include "../../../core/ops/Maths/CPU/math_cpu.h"
+
+#define EigenMatrixMap(dtype, data, rows, cols) \
+    Eigen::Map<Eigen::Matrix<dtype, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(data, rows, cols)
 
 void Forge::LinearCPU::forward(const Tensor& input, const Tensor& output, const Tensor& weights,
     const Tensor& bias, bool using_bias) const {
-    Tensor weights_T {{weights.shape()[1], weights.shape()[0]}, weights.dtype(), false};
-    Tensor bias_cast {output.shape(), bias.dtype(), false};
+
+    auto& idims {input.shape()};
+    auto& odims {output.shape()};
+
+    std::size_t M {1};
+    std::size_t N {weights.shape()[0]};
+    std::size_t K {weights.shape()[1]};
+
+    for (int i {}; i<idims.size()-1; ++i) M*=idims[i];
+
     DISPATCH_ALL_TYPES(input.dtype(), Device::CPU, [&] {
-        auto weights_map { weights.as_eigen<scalar_t>()};
-        auto input_map   { input.as_eigen<scalar_t>()  };
-        auto output_map  { output.as_eigen<scalar_t>() };
-        auto weights_T_map {weights_T.as_eigen<scalar_t>()};
+        forge_contract<scalar_t>(static_cast<scalar_t*>(input.data()), static_cast<scalar_t*>(weights.data()),
+            static_cast<scalar_t*>(output.data()), M, N, K, false, true, true);
 
-        Eigen::array<std::size_t , 4> shuffling {0, 1, 3, 2};
-        forge_eval(weights_T_map, weights_map.shuffle(shuffling));
+        // auto weights_map { weights.as_eigen<scalar_t>()};
+        // auto input_map   { input.as_eigen<scalar_t>()  };
+        //auto output_map  { output.as_eigen<scalar_t>() };
+        // Eigen::array<std::size_t , 4> shuffling {0, 1, 3, 2};
+        // Eigen::array<Eigen::IndexPair<std::size_t>, 1> contraction_dims {Eigen::IndexPair<std::size_t>(3, 2)};
+        // auto weights_map { weights.as_eigen<scalar_t>()};
+        // auto input_map   { input.as_eigen<scalar_t>()  };
+        // auto output_map  { output.as_eigen<scalar_t>() };
 
-        Eigen::array<Eigen::IndexPair<std::size_t>, 1> contraction_dims {Eigen::IndexPair<std::size_t>(3, 2)};
-        auto contract {input_map.contract(weights_T_map,
-            contraction_dims).reshape(output_map.dimensions()).eval()};
-
-        forge_eval(output_map, contract);
+        // forge_eval(output_map, input_map.contract(weights_map.shuffle(shuffling),
+        //     contraction_dims).reshape(output_map.dimensions()));
 
         if (using_bias) {
+            auto output_map  { output.as_eigen<scalar_t>() };
             auto bias_map    { bias.as_eigen<scalar_t>() };
-            auto bias_bcast_map  {bias_cast.as_eigen<scalar_t>()};
-            auto opt_dims {output_map.dimensions()};
+            auto odims {output_map.dimensions()};
 
             assert(bias_map.dimension(0) == 1 && bias_map.dimension(1) == 1 &&
                 bias_map.dimension(2) == 1 && "bias must be (1,1,1,output_size)");
 
-            Eigen::array<Eigen::Index , 4> broadcast_dims {opt_dims[0], opt_dims[1], opt_dims[2], 1};
+            Eigen::array<Eigen::Index , 4> broadcast_dims {odims[0], odims[1], odims[2], 1};
 
-            forge_eval (bias_bcast_map, output_map+bias_map.broadcast(broadcast_dims).eval());
-            forge_eval(output_map, bias_bcast_map);
+            forge_eval(output_map, output_map+bias_map.broadcast(broadcast_dims));
         }
     });
 }
