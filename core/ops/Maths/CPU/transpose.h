a4a26348c18b9c8f6c4d03d61e59e9a1c5ad5d7f
muchlakshay <reduxfusion@gmail.com>
2026-07-13 23:01:03 +0530

    forward declare transpose kernel (avx2)

--- core/ops/Maths/CPU/transpose.h
+++ core/ops/Maths/CPU/transpose.h
@@ -3,6 +3,7 @@
 
 namespace Forge {
     std::vector<std::size_t> compute_strides(const std::vector<std::size_t>& shape);
+    void transpose_AVX2(Tensor& A, Tensor B, const std::vector<std::size_t>& transpose_axis);
 }
 
 inline std::vector<std::size_t> Forge::compute_strides(const std::vector<std::size_t> &shape) {
