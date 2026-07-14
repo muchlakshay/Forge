3850c3847bf459ad5593833b205b9fc9fc9ac1a0
muchlakshay <reduxfusion@gmail.com>
2026-07-14 23:11:50 +0530

    implement `precalculate_offsets()` function

--- core/ops/Maths/CPU/transpose.h
+++ core/ops/Maths/CPU/transpose.h
@@ -4,6 +4,9 @@
 namespace Forge {
     std::vector<std::size_t> compute_strides(const std::vector<std::size_t>& shape);
     void transpose_AVX2(Tensor& A, Tensor B, const std::vector<std::size_t>& transpose_axis);
+    std::vector<int> precalculate_offsets(
+        const std::vector<std::size_t>& strides_A, const std::vector<std::size_t>& strides_B,
+        const std::vector<int>& permutation, std::size_t size);
 }
 
 inline std::vector<std::size_t> Forge::compute_strides(const std::vector<std::size_t> &shape) {
@@ -15,3 +18,23 @@ inline std::vector<std::size_t> Forge::compute_strides(const std::vector<std::si
     }
     return strides;
 }
+
+inline std::vector<int> Forge::precalculate_offsets(const std::vector<std::size_t> &strides_A,
+    const std::vector<std::size_t>& strides_B, const std::vector<int>& permutation, std::size_t size) {
+    std::vector<int> offsets (size);
+    auto rank {strides.size()};
+    std::vector<int> coords_B(rank);
+    std::vector<int> coords_A(rank);
+
+    for (int i {}; i<size; i++) {
+        int flat_idx {i}, src_flat_idx {};
+        for (int d{}; d<rank; d++) {
+            coords_B[d] = flat_idx/strides_B[d];
+            flat_idx %= strides_B[d];
+            coords_A[permutation[d]] = coords_B[d];
+            src_flat_idx += coords_A[d] * strides_A[d];
+        }
+        offsets[i] = src_flat_idx;
+    }
+}
+
