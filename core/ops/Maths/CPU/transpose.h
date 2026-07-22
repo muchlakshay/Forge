d6994320dfb093d58d158e74a2a295028666603c
muchlakshay <reduxfusion@gmail.com>
2026-07-22 22:47:44 +0530

    use assignment op in for loop, 'cuase direct list init was causing an error

--- core/ops/Maths/CPU/transpose.h
+++ core/ops/Maths/CPU/transpose.h
@@ -46,15 +46,15 @@ inline std::vector<int> Forge::precalculate_offsets(const std::vector<std::size_
 template<typename DTYPE>
 void Forge::transpose_AVX2(const DTYPE *A, DTYPE *B, const std::vector<std::size_t> &strides_A,
     const std::vector<std::size_t> &shape_A, const std::vector<int> &permutation, std::size_t size) {
-    int step {8};
+    std::size_t step {8};
     std::size_t end {size-step};
     std::vector<std::size_t> shape_B;
     for (int i{}; i<shape_A.size(); i++) shape_B.push_back(shape_A[permutation[i]]);
     auto strides_B {compute_strides(shape_B)};
     const auto offsets {precalculate_offsets(strides_A, strides_B, permutation, size)};
 
-#pragma omp parallel for schedule(static)
-    for (int i {}; i<=end; i+=step) {
+    #pragma omp parallel for schedule(static)
+    for (std::size_t i =0; i<=end; i+=step) {
         __m256i v_offsets {_mm256_loadu_si256(reinterpret_cast<const __m256i *>(&(offsets[i])))};
         __m256 v_data {_mm256_i32gather_ps(A, v_offsets, 4)};
         _mm256_storeu_ps(B+i, v_data);
