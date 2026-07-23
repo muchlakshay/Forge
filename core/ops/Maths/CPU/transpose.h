baf98bfcc931e3a7ebd6ffc8df640da050aed60c
muchlakshay <reduxfusion@gmail.com>
2026-07-23 21:45:25 +0530

    use type alias for std::vector<std::size_t> and add "forge_" prefix in transpose func name

--- core/ops/Maths/CPU/transpose.h
+++ core/ops/Maths/CPU/transpose.h
@@ -1,19 +1,21 @@
+#pragma once
 #include <immintrin.h>
 #include <tensor/tensor.h>
 
 namespace Forge {
-    std::vector<std::size_t> compute_strides(const std::vector<std::size_t>& shape);
+    using size_vec = std::vector<size_t>;
+    size_vec compute_strides(const size_vec& shape);
     template <typename DTYPE>
-    void transpose_AVX2(const DTYPE* A, DTYPE* B, const std::vector<std::size_t>& strides_A,
-        const std::vector<std::size_t>& shape_A, const std::vector<int>& permutation, std::size_t size);
+    void forge_transpose_AVX2(const DTYPE* A, DTYPE* B, const size_vec& strides_A,
+        const size_vec& shape_A, const std::vector<int>& permutation, std::size_t size);
     std::vector<int> precalculate_offsets(
-        const std::vector<std::size_t>& strides_A, const std::vector<std::size_t>& strides_B,
+        const size_vec& strides_A, const size_vec& strides_B,
         const std::vector<int>& permutation, std::size_t size);
 }
 
-inline std::vector<std::size_t> Forge::compute_strides(const std::vector<std::size_t> &shape) {
+inline Forge::size_vec Forge::compute_strides(const size_vec &shape) {
     std::size_t stride {1}, idx {shape.size()-1};
-    std::vector<std::size_t> strides (shape.size());
+    size_vec strides (shape.size());
     for (const auto dim : shape | std::views::reverse) {
         strides[idx--] = stride;
         stride *= dim;
@@ -21,8 +23,8 @@ inline std::vector<std::size_t> Forge::compute_strides(const std::vector<std::si
     return strides;
 }
 
-inline std::vector<int> Forge::precalculate_offsets(const std::vector<std::size_t> &strides_A,
-    const std::vector<std::size_t>& strides_B, const std::vector<int>& permutation, std::size_t size) {
+inline std::vector<int> Forge::precalculate_offsets(const size_vec &strides_A,
+    const size_vec& strides_B, const std::vector<int>& permutation, std::size_t size) {
     std::vector<int> offsets (size);
     auto rank {strides_A.size()};
     std::vector<int> coords_B(rank);
@@ -44,11 +46,11 @@ inline std::vector<int> Forge::precalculate_offsets(const std::vector<std::size_
 
 
 template<typename DTYPE>
-void Forge::transpose_AVX2(const DTYPE *A, DTYPE *B, const std::vector<std::size_t> &strides_A,
-    const std::vector<std::size_t> &shape_A, const std::vector<int> &permutation, std::size_t size) {
+void Forge::forge_transpose_AVX2(const DTYPE *A, DTYPE *B, const size_vec &strides_A,
+    const size_vec &shape_A, const std::vector<int> &permutation, std::size_t size) {
     std::size_t step {8};
     std::size_t end {size-step};
-    std::vector<std::size_t> shape_B;
+    size_vec shape_B;
     for (int i{}; i<shape_A.size(); i++) shape_B.push_back(shape_A[permutation[i]]);
     auto strides_B {compute_strides(shape_B)};
     const auto offsets {precalculate_offsets(strides_A, strides_B, permutation, size)};
