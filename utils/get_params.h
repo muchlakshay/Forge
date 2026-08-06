af4b59997c61650ff7c3682b4d0e0c9839553a0b
muchlakshay <reduxfusion@gmail.com>
2026-08-06 21:51:30 +0530

    implement `extract_parameters()` fn to automatically fetch parameters from data members of a model, using reflect-cpp

--- utils/get_params.h
+++ utils/get_params.h
@@ -12,3 +12,18 @@ namespace Forge {
     std::vector<Tensor*> extract_parameters(T& model);
 }
 
+template<typename T>
+std::vector<Forge::Tensor *> Forge::extract_parameters(T& model) {
+    auto data_members {rfl::to_view(model)};
+    std::vector<Tensor*> parameters;
+
+    rfl::apply(
+        [&](auto*... members) {
+            ([&]() {
+                if constexpr (HasParameters<std::remove_pointer_t<decltype(members)>>) {
+                    for (std::vector<Tensor*> params {members->parameters()}; auto p : params) {parameters.push_back(p);}
+                }
+            }(), ...);
+        }, data_members.values());
+    return parameters;
+}
