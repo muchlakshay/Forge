51f0b726ac67d244b1fe53ef4ebafb7594385f5c
muchlakshay <reduxfusion@gmail.com>
2026-08-06 22:18:08 +0530

    change `extract_parameters()` to fetch parameters into std::vector<Parameter>

--- utils/get_params.h
+++ utils/get_params.h
@@ -2,26 +2,27 @@
 #include "tensor.h"
 #include <rfl.hpp>
 #include <type_traits>
+#include "primitives/parameter.h"
 
 namespace Forge {
     template <typename T>
     concept HasParameters = requires(T x){
-        {x.parameters()}->std::same_as<std::vector<Tensor*>>;
+        {x.parameters()}->std::same_as<std::vector<Parameter>>;
     };
     template <typename T>
-    std::vector<Tensor*> extract_parameters(T& model);
+    std::vector<Parameter> extract_parameters(T& model);
 }
 
 template<typename T>
-std::vector<Forge::Tensor *> Forge::extract_parameters(T& model) {
+std::vector<Forge::Parameter> Forge::extract_parameters(T& model) {
     auto data_members {rfl::to_view(model)};
-    std::vector<Tensor*> parameters;
+    std::vector<Parameter> parameters;
 
     rfl::apply(
         [&](auto*... members) {
             ([&]() {
                 if constexpr (HasParameters<std::remove_pointer_t<decltype(members)>>) {
-                    for (std::vector<Tensor*> params {members->parameters()}; auto p : params) {parameters.push_back(p);}
+                    for (std::vector<Parameter> params {members->parameters()}; auto p : params) {parameters.push_back(p);}
                     for (auto p : extract_parameters(members)) parameters.push_back(p);
                 }
             }(), ...);
