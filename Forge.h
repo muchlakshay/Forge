bdfc19108fd641604127ac3b5a5c57b585406ab8
muchlakshay <reduxfusion@gmail.com>
2026-08-07 22:59:23 +0530

    include get_params.h

--- Forge.h
+++ Forge.h
@@ -1,5 +1,8 @@
 #pragma once
+
 #include "core/tensor/tensor.h"
 #include "primitives/Dispatcher/register_primitives.h"
 #include "primitives/Dispatcher/primitives.h"
-
+#include "Sinusoidal PE/sinusoidal_pe.h"
+#include "parameter.h"
+#include "utils/get_params.h"
