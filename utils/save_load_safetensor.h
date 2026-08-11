438efb1af5f3cb1d28b28a9461ad529779908885
muchlakshay <reduxfusion@gmail.com>
2026-08-11 18:18:23 +0530

    implement `get_dtype()` and `get_dtype_size()`

--- utils/save_load_safetensor.h
+++ utils/save_load_safetensor.h
@@ -18,5 +18,23 @@ namespace Forge {
         std::vector<std::size_t> data_offsets;
     };
 }
+inline std::string Forge::get_dtype(const Dtype dtype) {
+    switch (dtype) {
+        case Dtype::float16: return "F16";
+        case Dtype::float32: return "F32";
+        case Dtype::float64: return "F64";
+        default: throw std::invalid_argument("Unsupported dtype");
+    }
+}
+
+inline std::size_t Forge::get_dtype_size(Dtype dtype) {
+    switch (dtype) {
+        case Dtype::float16: return 2;
+        case Dtype::float32: return 4;
+        case Dtype::float64: return 8;
+        default: throw std::invalid_argument("Unsupported dtype");
+    }
+}
+
 
 
