02c1bb9ceccef6ebab252f7ede49e0e6550fbb63
muchlakshay <reduxfusion@gmail.com>
2026-08-16 18:47:21 +0530

    use `forge_max_AVX2()` in CE loss impl

--- primitives/Loss Functions/CPU/lossFunctionsCPU.cpp
+++ primitives/Loss Functions/CPU/lossFunctionsCPU.cpp
@@ -1,6 +1,7 @@
 #include "lossFunctionsCPU.h"
 #include "execution_ctx.h"
 #include <iostream>
+#include "ops/Maths/CPU/max.h"
 
 void Forge::MSE_CPU::compute_loss(const Tensor &pred, const Tensor &ground_truth, Tensor &loss) const {
     DISPATCH_ALL_TYPES(pred.dtype(), Device::CPU, [&] {
@@ -17,6 +18,7 @@ void Forge::CrossEntropyCPU::compute_loss(const Tensor &pred, const Tensor &grou
     Tensor log_softmax_eval {{pred.shape()}, pred.dtype(), false, pred.device()};
     Tensor row_max_eval {{pred.shape()}, pred.dtype(), false, pred.device()};
     Tensor logsumexp_eval {{pred.shape()}, pred.dtype(), false, pred.device()};
+    Tensor shifted_eval {pred.shape(), pred.dtype(), false, pred.device()};
 
     DISPATCH_ALL_TYPES(pred.dtype(), Device::CPU, [&] {
         auto pred_map {pred.as_eigen<scalar_t>()};
@@ -40,15 +42,23 @@ void Forge::CrossEntropyCPU::compute_loss(const Tensor &pred, const Tensor &grou
         Eigen::array<Eigen::Index, 4> bcast{1, 1, 1, dims[3]};
         Eigen::array<Eigen::Index, 1> batch_dim {2};
 
-        auto row_max {pred_map.maximum(class_dim).reshape(keep_dim).broadcast(bcast)};
+        auto r_max {forge_max_AVX2<scalar_t>(pred, pred.shape().size()-1)};
+        auto row_max {r_max.as_eigen<scalar_t>().broadcast(bcast)};
         forge_eval(row_max_eval_map, row_max);
 
+        auto shifted_eval_map {shifted_eval.as_eigen<scalar_t>()};
         auto shifted {pred_map - row_max_eval_map};
-        auto logsumexp {shifted.exp().sum(class_dim).log().reshape(keep_dim).broadcast(bcast)};
+        forge_eval(shifted_eval_map, shifted);
+        // forge_exp_AVX2(static_cast<scalar_t>(shifted_eval.data()), shifted_eval_map.data());
+
+        // auto start_out = std::chrono::steady_clock::now();
+        auto logsumexp {shifted_eval_map.exp().sum(class_dim).log().reshape(keep_dim).broadcast(bcast)};
         forge_eval(logsumexp_eval_map, logsumexp);
 
+        // auto end_out = std::chrono::steady_clock::now();
         forge_eval(log_softmax_eval_map, (shifted-logsumexp_eval_map));
 
+        // auto start_in = std::chrono::steady_clock::now();
         if (ground_truth.dtype() == Dtype::int32 && ground_truth.shape().size()==(pred.shape().size()-1)) {
 
             auto idx_map {ground_truth.as_eigen<int>()};
@@ -61,6 +71,9 @@ void Forge::CrossEntropyCPU::compute_loss(const Tensor &pred, const Tensor &grou
             Eigen::Tensor<scalar_t, 0, Eigen::RowMajor> NLL_val {};
             NLL_val.setValues({NLL});
             loss_map(0, 0, 0, 0) = static_cast<float>(NLL_val()) / static_cast<float>(idx_map.size());
+            auto end_in = std::chrono::steady_clock::now();
+            // std::cout<<"out: "<<(end_out-start_out).count()<<
+            //     "\nin: "<<(end_in-start_in).count()<<"\n";
             return;
         }
 
