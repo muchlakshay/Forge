d5aa93bc43c0b86065cd25309ffdb876c44c660b
muchlakshay <reduxfusion@gmail.com>
2026-08-21 22:10:19 +0530

    add reshape_grads enumerator in UtilityOps

--- core/enums.h
+++ core/enums.h
@@ -5,6 +5,6 @@ namespace Forge {
     enum class Dtype {float32, int32, DtypeCount};
     enum class Device {CPU, CUDA};
     enum class DispatchKey {CPU, CUDA, CPU_Autodiff, CUDA_Autodiff, DispatchKeysCount};
-    enum class UtilityOps {storage_backend, initializers, constant, print, storage_copy, random, bcast_add, Count};
+    enum class UtilityOps {storage_backend, initializers, constant, print, storage_copy, random, bcast_add, reshape_grads, Count};
     enum class Initializers{xavier_normal, xavier_uniform, he_normal, he_uniform};
 }
